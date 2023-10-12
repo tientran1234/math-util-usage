e2b41ee3587f01092f928c70edcc6ea0acbc9b4c
tientran1234 <tranngoctien29112003@gmail.com>
2023-10-12 14:39:59 +0700

    1st upload

---
+++ src/main/java/com/trantien/mathutil/usage/main/MathUtilUsage.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.trantien.mathutil.usage.main;
+
+import com.trantien.mathuntil.core.MathUtility;
+
+/**
+ *
+ * @author mac
+ */
+public class MathUtilUsage {
+
+    public static void main(String[] args) {
+        System.out.println("Using my own library");
+        System.out.println("6! = 720 | actual: " + MathUtility.getFactorial(6));
+    }
+}
